cc48e837ab500bb4584193e25719cf3c1eca8df7
Jaykumar Sanandiya <jaykumarsanandiya@gmail.com>
2022-03-30 15:41:08 +0530

    image add with download

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+
+@section('content')
+
+<div class="container">
+    <h1>Login Page</h1>
+    <form action="/session/login" method="POST">
+        @csrf
+        <input type="text" name="username" placeholder="enter username"><br><br>
+        <input type="password" name="password" placeholder="enter password"><br><br>
+        <button type="submit">Login</button>
+    </form>
+    
+</div>
+
+
+  @endsection
